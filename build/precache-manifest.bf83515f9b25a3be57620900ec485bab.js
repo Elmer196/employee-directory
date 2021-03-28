1693d46bd5113421f556e75c88c3bba6076de758
Elmer196 <elmerkobe88@gmail.com>
2021-03-28 17:41:29 -0500

    Packges installed

---
+++ build/precache-manifest.bf83515f9b25a3be57620900ec485bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbc7bd911339ad371d06dc169a7ac9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b222bd0b95409c69e406",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "92fd3daea40d5dcdb094",
+    "url": "/static/css/main.3ae1f97e.chunk.css"
+  },
+  {
+    "revision": "b222bd0b95409c69e406",
+    "url": "/static/js/2.e3fa96f2.chunk.js"
+  },
+  {
+    "revision": "92fd3daea40d5dcdb094",
+    "url": "/static/js/main.e49f8985.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
